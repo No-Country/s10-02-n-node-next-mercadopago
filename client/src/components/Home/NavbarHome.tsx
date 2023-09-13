12cbfcba1eab62299ef34d1ddef1a28eda085844
Adri-ESM <anggivmorales@gmail.com>
2023-09-13 11:33:26 -0300

    fixing some things in the styles of profileCard

--- client/src/components/Home/NavbarHome.tsx
+++ client/src/components/Home/NavbarHome.tsx
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation'
 import { io } from 'socket.io-client'
 import { useUserProfile } from '@/store/userStore'
 
-export default function NavbarHome() {
+export default function NavbarHome () {
   const pathname = usePathname()
 
   const routesAndTitles = [
@@ -19,18 +19,18 @@ export default function NavbarHome() {
     { path: '/send-money/deposit-money', title: 'Enviar dinero' },
     {
       path: '/send-money/pay-method',
-      title: 'Enviar dinero',
+      title: 'Enviar dinero'
     },
     {
       path: '/send-money/search-user',
-      title: 'Enviar dinero',
+      title: 'Enviar dinero'
     },
     { path: '/spei', title: 'Enviar dinero' },
     { path: '/money-charge', title: 'Ingresar dinero' },
-    { path: '/money-charge/transfer-debit', title: 'Ingresar dinero' },
+    { path: '/money-charge/transfer-debit', title: 'Ingresar dinero' }
   ]
 
-  const matchedRoute = routesAndTitles.find((route) => route.path === pathname)
+  const matchedRoute = routesAndTitles.find(route => route.path === pathname)
   const pageTitle = matchedRoute ? matchedRoute.title : ''
 
   //---------------
@@ -43,7 +43,7 @@ export default function NavbarHome() {
   const displayNotification = (msg: string) => {
     return (
       <span
-        className="p-2 text-white bg-green-400 rounded-md cursor-pointer"
+        className='p-2 text-white bg-green-400 rounded-md cursor-pointer'
         onClick={handleRead}
       >
         {msg}
@@ -59,7 +59,7 @@ export default function NavbarHome() {
   // --------- Handle sockets ----------
   useEffect(() => {
     const socket = io(
-      'https://s10-02-n-node-next-mercadopago-production.up.railway.app/',
+      'https://s10-02-n-node-next-mercadopago-production.up.railway.app/'
     )
 
     setMySocket(socket)
@@ -78,23 +78,23 @@ export default function NavbarHome() {
 
   return (
     <>
-      <header className="relative flex items-center justify-between w-full h-16 px-6 bg-tertiary">
+      <header className='relative flex items-center justify-between w-full h-16 px-6 bg-tertiary'>
         {/* Titulo de la pagina */}
-        <p className="text-lg font-bold text-white">{pageTitle}</p>
+        <p className='text-lg font-bold text-white'>{pageTitle}</p>
         {/* Icono de ayuda */}
         <figure
-          className="relative w-6 h-6 cursor-pointer"
+          className='relative w-6 h-6 cursor-pointer'
           onClick={() => setOpen(!open)}
         >
-          <Image src={bell} alt="bell" />
+          <Image src={bell} alt='bell' />
           {notifications.length > 0 && (
-            <div className="absolute flex items-center justify-center w-5 h-5 text-sm font-semibold text-white bg-red-500 rounded-full left-[40%] top-[-40%]">
+            <div className='absolute flex items-center justify-center w-5 h-5 text-sm font-semibold text-white bg-red-500 rounded-full left-[40%] top-[-40%]'>
               {notifications.length}
             </div>
           )}
         </figure>
         {open && (
-          <div className="absolute top-[105%] right-4 flex flex-col gap-1 items-end">
+          <div className='absolute top-[105%] right-4 flex flex-col gap-1 items-end'>
             {notifications.map((n: string) => displayNotification(n))}
           </div>
         )}
